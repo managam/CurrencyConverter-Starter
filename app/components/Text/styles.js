03402236864c309f3ebe968914377be63ee2f1e9
Managam Silalahi <managam.silalahi@gmail.com>
2017-08-09 13:58:02 +0700

    http://learn.handlebarlabs.com/courses/react-native-basics-build-a-currency-converter/lectures/2643154 completed

---
+++ app/components/Text/styles.js
@@ -0,0 +1,12 @@
+import EStyleSheet from "react-native-extended-stylesheet";
+import StyleSheet from "react-native";
+
+const styles = EStyleSheet.create({
+  smallText: {
+    color: "$white",
+    fontSize: 12,
+    textAlign: "center"
+  }
+});
+
+export default styles;
--- app/index.js
+++ app/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import EStyleSheet from 'react-native-extended-stylesheet';
-import Home from './screens/Home';
+import React from "react";
+import EStyleSheet from "react-native-extended-stylesheet";
+import Home from "./screens/Home";
 
 EStyleSheet.build({
-  $primaryBlue: '#4F6D7A',
-  $secondYellow: 'yellow',
-  $white: '#fff',
-  $border: '#e2e2e2',
-  $inputText: '#797979',
-  $lightGray: '#f0f0f0'
+  $primaryBlue: "#4F6D7A",
+  $secondYellow: "yellow",
+  $white: "#fff",
+  $border: "#e2e2e2",
+  $inputText: "#797979",
+  $lightGray: "#f0f0f0"
 });
 
 export default () => <Home />;
