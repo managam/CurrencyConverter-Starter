94949f57fa8d50caf1d3d11de02dcb84f662f50c
Managam Silalahi <managam.silalahi@gmail.com>
2017-08-09 13:36:07 +0700

    http://learn.handlebarlabs.com/courses/react-native-basics-build-a-currency-converter/lectures/2643153 completed

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,47 +1,56 @@
-import React, { Component } from 'react';
-import { Text, View, StatusBar } from 'react-native';
+import React, { Component } from "react";
+import { Text, View, StatusBar } from "react-native";
 
-import { Container } from '../components/Container';
-import { Logo } from '../components/Logo';
-import { InputWithButton } from '../components/TextInput';
+import { Container } from "../components/Container";
+import { Logo } from "../components/Logo";
+import { InputWithButton } from "../components/TextInput";
+import { ClearButton } from "../components/Buttons";
 
-const TEMP_BASE_CURRENCY = 'USD';
-const TEMP_QUOTE_CURRENCY = 'GBP';
-const TEMP_BASE_PRICE = '100';
-const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_BASE_CURRENCY = "USD";
+const TEMP_QUOTE_CURRENCY = "GBP";
+const TEMP_BASE_PRICE = "100";
+const TEMP_QUOTE_PRICE = "79.74";
 
 class Home extends Component {
   handlePressBaseCurrency = () => {
-    console.log('Press base');
-  }
+    console.log("Press base");
+  };
 
   handlePressQuoteCurrency = () => {
-    console.log('Press quote');
-  }
+    console.log("Press quote");
+  };
 
-  handleTextChange = (text) => {
-    console.log('Change text', text);
-  }
+  handleTextChange = text => {
+    console.log("Change text", text);
+  };
+
+  handleSwapCurrency = () => {
+    console.log("Press swap");
+  };
 
   render() {
     return (
-    <Container>
-      <StatusBar translucent={false} barStyle="light-content" />
-      <Logo />
-      <InputWithButton
-        buttonText={TEMP_BASE_CURRENCY} 
-        onPress={this.handlePressBaseCurrency}
-        defaultValue={TEMP_BASE_PRICE}
-        keyboardType="numeric"
-        onChangeText={this.handleTextChange}
-      />
-      <InputWithButton 
-        buttonText={TEMP_QUOTE_CURRENCY}
-        onPress={this.handlePressQuoteCurrency}
-        editable={false}
-        value={TEMP_QUOTE_PRICE}
-      />
-    </Container>
+      <Container>
+        <StatusBar translucent={false} barStyle="light-content" />
+        <Logo />
+        <InputWithButton
+          buttonText={TEMP_BASE_CURRENCY}
+          onPress={this.handlePressBaseCurrency}
+          defaultValue={TEMP_BASE_PRICE}
+          keyboardType="numeric"
+          onChangeText={this.handleTextChange}
+        />
+        <InputWithButton
+          buttonText={TEMP_QUOTE_CURRENCY}
+          onPress={this.handlePressQuoteCurrency}
+          editable={false}
+          value={TEMP_QUOTE_PRICE}
+        />
+        <ClearButton
+          text="Reverse Currencies"
+          onPress={this.handleSwapCurrency}
+        />
+      </Container>
     );
   }
 }
