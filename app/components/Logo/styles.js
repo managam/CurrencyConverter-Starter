33022bbfa326fe2ccbe1aa824c9d0ad7e7779472
admin <managam.silalahi@gmail.com>
2017-08-10 13:55:31 +0700

    http://learn.handlebarlabs.com/courses/react-native-basics-build-a-currency-converter/lectures/2643157 done

--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -4,17 +4,24 @@ import { Dimensions } from 'react-native';
 const imageWidth = Dimensions.get('window').width / 2;
 
 const styles = EStyleSheet.create({
+  
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
+
   container: {
     alignItems: 'center',
   },
   containerImage: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: imageWidth,
-    height: imageWidth,
+    width: "$largeContainerSize",
+    height: "$largeContainerSize",
   },
   image: {
-    width: imageWidth / 2,
+    width: "$largeImageSize",
+    tintColor: "$primaryBlue"
   },
   text: {
     fontWeight: '600',
