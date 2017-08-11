618e32a9d94faeeb80263cbb1eb28b26b41c0d85
Managam Silalahi <managam.silalahi@gmail.com>
2017-08-11 13:51:01 +0700

    http://learn.handlebarlabs.com/courses/react-native-basics-build-a-currency-converter/lectures/2643162 done

--- app/components/List/Icon.js
+++ app/components/List/Icon.js
@@ -1,23 +1,34 @@
-import React, {PropTypes} from "react";
-import {View, Image} from "react-native";
+import React, { PropTypes } from "react";
+import { View, Image } from "react-native";
 import styles from "./styles";
 
-const Icon = ({checkmark, visible}) => {
-    const iconStyles = [styles.icon];
-    if (visible) {
-        iconStyles.push(styles.iconVisible);
-    }
+const Icon = ({ checkmark, visible, iconBackground }) => {
+  const iconStyles = [styles.icon];
+  if (visible) {
+    iconStyles.push(styles.iconVisible);
+  }
 
-    return (
-        <View style={iconStyles}>
-            {checkmark ? <Image style={styles.checkIcon} resizeMode="contain" source={require("./images/check.png")} /> : null}
-        </View>
-    );
+  if (iconBackground) {
+    iconStyles.push({ backgroundColor: iconBackground });
+  }
+
+  return (
+    <View style={iconStyles}>
+      {checkmark
+        ? <Image
+            style={styles.checkIcon}
+            resizeMode="contain"
+            source={require("./images/check.png")}
+          />
+        : null}
+    </View>
+  );
 };
 
 Icon.propTypes = {
-    checkmark: PropTypes.bool,
-    visible: PropTypes.bool
-}
+  checkmark: PropTypes.bool,
+  visible: PropTypes.bool,
+  iconBackground: PropTypes.string
+};
 
 export default Icon;
--- app/index.js
+++ app/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import EStyleSheet from "react-native-extended-stylesheet";
 //import Home from "./screens/Home";
 //import CurrencyList from "./screens/CurrencyList";
-import Options from "./screens/Options";
+//import Options from "./screens/Options";
+import Themes from "./screens/Themes";
 
 EStyleSheet.build({
   $primaryBlue: "#4F6D7A",
+  $primaryOrange: "#d57a66",
+  $primaryGreen: "#00db9d",
+  $primaryPurple: "#9e768f",
   $secondYellow: "yellow",
   $white: "#fff",
   $border: "#e2e2e2",
@@ -14,4 +18,4 @@ EStyleSheet.build({
   $lightGray: "#f0f0f0"
 });
 
-export default () => <Options />;
+export default () => <Themes />;
